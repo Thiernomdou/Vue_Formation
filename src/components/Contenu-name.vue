3131795ff801c85a38c05858a3ff8fd168156732
DIALLO Thierno <thiernomdou63@gmail.com>
2023-03-21 22:11:50 +0100

    CSS dans un fichier separé

--- src/components/Contenu-name.vue
+++ src/components/Contenu-name.vue
@@ -1,15 +1,24 @@
 <template>
-    <div>{{ txt }}</div>
+    <div>
+        <p>{{ txt }}</p>
+        <liste></liste>
+    </div>
+    
 </template>
 
 
 <script>
+import Liste from './Liste/Liste-fruits.vue'
 
 export default {
+    name: 'Contenu-name',
     data() {
         return {
             txt : 'Je suis le contenu'
         }
+    },
+    components: {
+        'liste': Liste
     }
 }
 
---
+++ src/components/Liste/Liste-fruits.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="liste">
+        <li v-for="(fruit, index) in fruits" v-bind:key="index">{{ fruit }}</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'Liste-fruits',
+        data() {
+            return {
+                fruits : ["Tomate", "Papaye", "Mangue"]
+            }
+        }
+    }
+</script>
+
+<style scoped src="./Liste-fruits.css">
+
+</style>
